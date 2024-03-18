f52ef7a385366fb217aef98e68b746c7fed1cb27
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2024-03-18 15:12:06 +0100

    tomasek

--- test (1)/main.c
+++ test (1)/main.c
@@ -8,22 +8,6 @@ int main()
     srand(time(NULL));
     Band *bands;
     int bNumber;
-    printf("Give me a number of bands\n");
-    scanf("%d", &bNumber);
-    if ((bands = (Band *)malloc(sizeof(Band) * bNumber)) == NULL)
-    {
-        return -1;
-    }
-    fillBands(bands, bNumber);
-    FILE *file;
-    file= fopen("file.txt","w");
-    for (int i = 0; i < bNumber; i++)
-    {
-        printf("Band number %d:\n", i + 1);
-        PrintInfoAboutBand(bands[i]);
-        fprintf(file,"%s",GetInfoAboutBand(bands[i]));
-    }
-    fclose(file);
     int choice;
     do
     {
